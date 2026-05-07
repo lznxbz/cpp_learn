f0185b28a82d5722c159fcadc2afdc42d93d3e40
lznxbz <lznxbz@126.com>
2026-05-07 13:14:23 +0200

    encapsulate in class, and apply threads

--- holdem_6p.cpp
+++ holdem_6p.cpp
@@ -5,63 +5,26 @@
 #include <stdexcept>
 #include <map>
 #include <functional>
+#include <random>
+#include <thread>
+#include <ctime>
+#include <format>
 using namespace std;
 
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
 /* 
-usage: holdem_6p #players hole0 hole1 [community0 ...] 
+usage: holdem_6p #players hole0 hole1 [community0 ~ community4] 
 */
 
-list<int> card_pile;
-void init_card_pile() {
-    for (int i = 0; i < 36; i++) {
-        card_pile.push_back(i);
+
+int int_pow(int base, int exponent) {
+    if (exponent > 0) {
+        exponent --;
+        return base * int_pow(base, exponent);
+    } else {
+        return 1;
     }
 }
-
-array<array<int,2>,8> hole_cards;
-array<int,5> community_cards;
-void dealing_cards(const int n_people, const array<int, 7>& known_cards, int n_community_cards) {
-    // Take out from card pile
-    for (int i = 0; i < n_community_cards + 2; i++) {
-        int card_i = known_cards[i];
-        auto remove_counter = card_pile.remove(card_i);
-        if (remove_counter != 1) {
-            cout << remove_counter << " of cards removed!" << endl;
-            throw invalid_argument("Cards out of range, or duplication of cards.");
-        }
-    }
-
-    // Assign to me and community cards
-    for (int i = 0; i < n_community_cards; i++) {
-        community_cards[i] = known_cards[i + 2];
-    }
-    hole_cards[0][0] = known_cards[0]; hole_cards[0][1] = known_cards[1];
-    
-    // Take out and assign for the rest players
-    for (int i = 1; i < n_people; i++) {
-        for (int ii = 0; ii < 2; ii++){
-            // assign a random card
-            auto it_card = card_pile.begin();
-            int p_card = rand() % card_pile.size();
-            advance(it_card, p_card);
-            hole_cards[i][ii] = *it_card;
-            card_pile.erase(it_card);
-        }
-    }
-
-    // Show all of 5 community cards
-    for (int i = n_community_cards; i < 5; i++) {
-        auto it_card = card_pile.begin();
-        int p_card = rand() % card_pile.size();
-        advance(it_card, p_card);
-        community_cards[i] = *it_card;
-        card_pile.erase(it_card);
-    }
-}
-
-
-
 
 int get_card_index(const array<char,2>& card_v) {
     int suits_index;
@@ -109,7 +72,6 @@ int get_card_index(const array<char,2>& card_v) {
     return 4 * name_index + suits_index;
 }
 
-
 array<char,2> get_card_point(const int card_index) {
     int name_index = card_index / 4; 
     int suit_index = card_index % 4;
@@ -139,16 +101,81 @@ array<char,2> get_card_point(const int card_index) {
     return card_point;
 }
 
-int int_pow(int base, int exponent) {
-    if (exponent > 0) {
-        exponent --;
-        return base * int_pow(base, exponent);
-    } else {
-        return 1;
+
+
+class Holdem {
+    private:
+        list<int> card_pile;
+        void init_card_pile();
+        array<array<int,2>,8> hole_cards;
+        array<int,5> community_cards;
+        void dealing_cards(mt19937& gen);
+
+    public:
+        int n_players;
+        int n_community_cards;
+        int* pknown_cards;
+        int calc_score(int hole0, int hole1);
+        int n_games = 1000;
+        array<double,8> probability;
+        void get_probability(mt19937& gen);
+        void test_game(mt19937& gen);
+
+};
+
+
+
+void Holdem::init_card_pile() {
+    for (int i = 0; i < 36; i++) {
+        card_pile.push_back(i);
     }
 }
 
-int calc_score(int hole0, int hole1) {
+void Holdem::dealing_cards(mt19937& gen) {
+    // Take out from card pile
+    for (int i = 0; i < n_community_cards + 2; i++) {
+        int card_i = pknown_cards[i];
+        auto remove_counter = card_pile.remove(card_i);
+        if (remove_counter != 1) {
+            cout << remove_counter << " of cards removed!" << endl;
+            throw invalid_argument("Cards out of range, or duplication of cards.");
+        }
+    }
+
+    // Assign to me and community cards
+    for (int i = 0; i < n_community_cards; i++) {
+        community_cards[i] = pknown_cards[i + 2];
+    }
+    hole_cards[0][0] = pknown_cards[0]; hole_cards[0][1] = pknown_cards[1];
+    
+    // Take out and assign for the rest players
+    for (int i = 1; i < n_players; i++) {
+        for (int ii = 0; ii < 2; ii++){
+            // assign a random card
+            auto it_card = card_pile.begin();
+            uniform_int_distribution<int> dist(0, card_pile.size() - 1);
+            int p_card = dist(gen);
+            advance(it_card, p_card);
+            hole_cards[i][ii] = *it_card;
+            card_pile.erase(it_card);
+        }
+    }
+
+    // Show all of 5 community cards
+    for (int i = n_community_cards; i < 5; i++) {
+        auto it_card = card_pile.begin();
+        uniform_int_distribution<int> dist(0, card_pile.size() - 1);
+        int p_card = dist(gen);
+        advance(it_card, p_card);
+        community_cards[i] = *it_card;
+        card_pile.erase(it_card);
+    }
+}
+
+
+
+
+int Holdem::calc_score(int hole0, int hole1) {
     array<int, 7> full_hands;
     for (int i = 0; i < 5; i++) full_hands[i] = community_cards[i];
     full_hands[5] = hole0; full_hands[6] = hole1;
@@ -285,6 +312,84 @@ int calc_score(int hole0, int hole1) {
 }
 
 
+void Holdem::get_probability (mt19937& gen) {
+    array<int,8> rank_counter{};
+    for (int match_i = 0; match_i < n_games; match_i++) { // each match
+        init_card_pile();
+        dealing_cards(gen);
+
+        // Probability
+        int my_score = calc_score(hole_cards[0][0], hole_cards[0][1]);
+        int my_rank = 0;
+        for (int i = 1; i < n_players; i++) {
+            if (my_score < calc_score(hole_cards[i][0], hole_cards[i][1])) my_rank++;
+        }
+        rank_counter[my_rank] ++;
+    }
+
+
+    for (int i = 0; i < n_players; i++) {
+        probability[i] = (double)rank_counter[i] / (double)n_games;
+    }
+}
+
+void Holdem::test_game(mt19937& gen) {
+        // TEST
+        init_card_pile();
+        dealing_cards(gen);
+        cout << "community: ";
+        // for (auto i : community_cards) {cout << i << " "; get_card_point(i); cout << endl; }
+        for (auto i : community_cards) cout << get_card_point(i)[0] << get_card_point(i)[1] << " "; cout << endl;
+
+        for (int i = 0; i < n_players; i++) {
+            cout << "player" << i << ": ";
+            // for (auto ii : hole_cards[i]) cout << ii << " "; cout << endl;
+            for (auto ii : hole_cards[i]) cout << get_card_point(ii)[0] << get_card_point(ii)[1] << " ";
+            cout << "Score: " << calc_score(hole_cards[i][0], hole_cards[i][1]);
+            cout << endl;
+        }
+}
+
+
+void worker_thread(
+    int n_players, 
+    int n_community_cards, 
+    int* pknown_cards,
+    double* rank_probabilities
+) {
+    // Random number generator for each thread. 
+    random_device rd;
+    mt19937 gen(rd());
+    
+    // Create a Holdem game
+    Holdem gameHoldem;
+    gameHoldem.n_players = n_players;
+    gameHoldem.n_community_cards = n_community_cards;
+    gameHoldem.pknown_cards = pknown_cards;
+    gameHoldem.get_probability(gen);
+
+    // Output results
+    for (int i = 0; i < gameHoldem.n_players; i++) {
+        rank_probabilities[i] += gameHoldem.probability[i];
+    }
+}
+
+void test_thread (
+    int n_players, 
+    int n_community_cards, 
+    int* pknown_cards
+) {
+    // Random number generator for each thread. 
+    random_device rd;
+    mt19937 gen(rd());
+    
+    // Create a Holdem game
+    Holdem gameHoldem;
+    gameHoldem.n_players = n_players;
+    gameHoldem.n_community_cards = n_community_cards;
+    gameHoldem.pknown_cards = pknown_cards;
+    gameHoldem.test_game(gen);
+}
 
 
 int main(int argc, char* argv[]) {
@@ -312,40 +417,24 @@ int main(int argc, char* argv[]) {
     int n_community_cards = argc - 4;
 
 
-    array<int,8> rank_counter{};
-    for (int match_i = 0; match_i < 1000; match_i++) { // each match
-        init_card_pile();
-        dealing_cards(n_people, cards_arr, n_community_cards);
+    double total_prob[8];
+    thread t1(worker_thread, n_people, n_community_cards, cards_arr.data(), total_prob);
+    thread t2(worker_thread, n_people, n_community_cards, cards_arr.data(), total_prob);
+    thread t3(worker_thread, n_people, n_community_cards, cards_arr.data(), total_prob);
+    thread t4(worker_thread, n_people, n_community_cards, cards_arr.data(), total_prob);
 
-        /* 
-        // TEST
-        cout << "community: ";
-        // for (auto i : community_cards) {cout << i << " "; get_card_point(i); cout << endl; }
-        for (auto i : community_cards) cout << get_card_point(i)[0] << get_card_point(i)[1] << " "; cout << endl;
+    // thread t1(test_thread, n_people, n_community_cards, cards_arr.data());
+    // thread t2(test_thread, n_people, n_community_cards, cards_arr.data());
+    // thread t3(test_thread, n_people, n_community_cards, cards_arr.data());
+    // thread t4(test_thread, n_people, n_community_cards, cards_arr.data());
 
-        for (int i = 0; i < n_people; i++) {
-            cout << "player" << i << ": ";
-            // for (auto ii : hole_cards[i]) cout << ii << " "; cout << endl;
-            for (auto ii : hole_cards[i]) cout << get_card_point(ii)[0] << get_card_point(ii)[1] << " ";
-            cout << "Score: " << calc_score(hole_cards[i][0], hole_cards[i][1]);
-            cout << endl;
-        }
-        */
-       
-        // Probability
-        int my_score = calc_score(hole_cards[0][0], hole_cards[0][1]);
-        int my_rank = 0;
-        for (int i = 1; i < n_people; i++) {
-            if (my_score < calc_score(hole_cards[i][0], hole_cards[i][1])) my_rank++;
-        }
-        rank_counter[my_rank] ++;
+    t1.join(); t2.join(); t3.join(); t4.join(); 
+
+    for (int i = 0; i < n_people; i++) {
+        double prob = total_prob[i] / 4.0 * 100.0;
+        cout << format("{:.2f}%; ", prob);
     }
-
-    for (int i = 0; i < n_people; i++) cout << rank_counter[i] << " ";
     cout << endl;
-
-
-
 
 }
 
